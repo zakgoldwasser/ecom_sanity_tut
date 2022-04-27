300a2115e56c2c2aa436a10ea6f2e75dec4294f0
unknown <zak.goldwasser@gmail.com>
2022-04-27 18:26:58 -0400

    first commit

---
+++ pages/success.js
@@ -0,0 +1,42 @@
+import React, {useState,useEffect} from 'react'
+import Link from 'next/link'
+import {BsBagCheckFill} from 'react-icons/bs'
+
+import { useStateContext } from '../context/StateContext'
+import { runConfetti } from '../lib/utils'
+
+const Success = () => {
+    const {setCartItems, setTotalPrice, setTotalQuantities}=useStateContext()
+
+    useEffect(()=>{
+        localStorage.clear()
+        setCartItems([])
+        setTotalPrice(0)
+        setTotalQuantities(0)
+        runConfetti()
+    },[])
+
+  return (
+    <div className='success-wrapper'>
+        <div className='success'>
+            <p className="icon">
+                <BsBagCheckFill/>
+            </p>
+            <h2>Thank you for our order</h2>
+            <p className='email-msg'>Check your email for your receipt</p>
+            <p className='description'>Have a problem? Email
+                <a className='email' href='mailto:order@example.com'>order@example.com</a>
+            </p>
+            <Link href="/">
+                <a>
+                <button type="button" width="300px" className='btn'>
+                    Continue Shopping
+                </button>
+                </a>
+            </Link>
+        </div>
+    </div>
+  )
+}
+
+export default Success
